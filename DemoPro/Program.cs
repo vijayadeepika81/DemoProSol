09b3a8c763ffc92d416506817e8aa93109f2a74e
Deepika <Deepika@DESKTOP-ELH0T5Q>
2024-05-20 11:53:43 +0530

    Add project files.

---
+++ DemoPro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DemoPro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, res;
+            a = 10;
+            b = 20;
+            res = a + b;
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
